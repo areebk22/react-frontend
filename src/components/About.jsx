3c1766e5ca0758a5044bc03eb82bf0a1947212a6
areebk22 <100992225+areebk22@users.noreply.github.com>
2024-07-10 23:21:30 +0530

    Add files via upload

---
+++ src/components/About.jsx
@@ -0,0 +1,94 @@
+import React from 'react';
+import './About.css';
+
+const About = () => {
+  return (
+    <div className="about-container">
+      <h1>About</h1>
+      <div className="section">
+        <div className="text">
+          <h2>Header1</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliquaLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliquaLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliquaLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+        </div>
+        <div className="image">
+          <img src="./docimg.png" alt="Header1" />
+        </div>
+      </div>
+      <div className="section">
+        <div className="image">
+          <img src="./docimg.png" alt="Header2" />
+        </div>
+        <div className="text">
+          <h2>Header2</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliquaLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliquaLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliquaLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+        </div>
+      </div>
+      <div className="section">
+        <div className="text">
+          <h2>Header3</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliquaLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliquaLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliquaLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+        </div>
+        <div className="image">
+          <img src="./docimg.png" alt="Header3" />
+        </div>
+      </div>
+      <div className="section">
+        <div className="image">
+          <img src="./docimg.png" alt="Header4" />
+        </div>
+        <div className="text">
+          <h2>Header4</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliquaLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliquaLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliquaLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
+
+// import React from 'react';
+// import './About.css';
+
+
+// const About = () => {
+//   return (
+//     <div className="about-container">
+//       <h1>About</h1>
+//       <div className="section">
+//         <div className="text">
+//           <h2>Header1</h2>
+//         </div>
+//         <div className="image">
+//           <img src="./docimg.png" alt="Header1" />
+//         </div>
+//       </div>
+//       <div className="section">
+//         <div className="image">
+//           <img src="./docimg.png" alt="Header2" />
+//         </div>
+//         <div className="text">
+//           <h2>Header2</h2>
+//         </div>
+//       </div>
+//       <div className="section">
+//         <div className="text">
+//           <h2>Header3</h2>
+//         </div>
+//         <div className="image">
+//           <img src="./docimg.png" alt="Header3" />
+//         </div>
+//       </div>
+//       <div className="section">
+//         <div className="image">
+//           <img src="./docimg.png" alt="Header4" />
+//         </div>
+//         <div className="text">
+//           <h2>Header4</h2>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default About;
